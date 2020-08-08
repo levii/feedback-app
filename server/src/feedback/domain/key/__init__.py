26a4b479cd9ac22a024e0247eb531721d7f37ff4
Akira Yumiyama <yumiyama.akira@gmail.com>
2020-08-09 08:59:05 +0900

    FeedbackKey, FeedbackCommentKey が値を保持できるようにする

--- server/src/feedback/domain/key/__init__.py
+++ server/src/feedback/domain/key/__init__.py
@@ -1,11 +1,36 @@
 import dataclasses
+import uuid
+from typing import ClassVar, List
 
 
 @dataclasses.dataclass(frozen=True)
 class FeedbackKey:
-    pass
+    feedback_id: str
+
+    KINDS: ClassVar[List[str]] = ["Feedback"]
+
+    @classmethod
+    def build(cls, feedback_id: str) -> "FeedbackKey":
+        return cls(feedback_id)
+
+    @classmethod
+    def build_new(cls) -> "FeedbackKey":
+        return cls.build(str(uuid.uuid4()))
 
 
 @dataclasses.dataclass(frozen=True)
 class FeedbackCommentKey:
-    pass
+    feedback_id: str
+    comment_id: str
+
+    KINDS: ClassVar[List[str]] = ["Feedback", "FeedbackComment"]
+
+    @classmethod
+    def build(cls, feedback_id: str, comment_id: str) -> "FeedbackCommentKey":
+        return cls(feedback_id, comment_id)
+
+    @classmethod
+    def build_new(cls, feedback_key: FeedbackKey) -> "FeedbackCommentKey":
+        return cls.build(
+            feedback_id=feedback_key.feedback_id, comment_id=str(uuid.uuid4())
+        )
