6524b0d4dddece61992c4d0f56403d7efa9ccde2
Akira Yumiyama <yumiyama.akira@gmail.com>
2020-08-11 15:31:12 +0900

    FeedbackCommentUser の属性を追加 (name, icon_url)

--- server/src/feedback/domain/comment/__init__.py
+++ server/src/feedback/domain/comment/__init__.py
@@ -2,12 +2,19 @@ import dataclasses
 from typing import List
 
 from common.user.domain.key import UserKey
+from common.user.domain.user import UserName, UserIconURL, User
 from feedback.domain.key import FeedbackCommentKey
 
 
 @dataclasses.dataclass(frozen=True)
 class FeedbackCommentUser:
     user_key: UserKey
+    name: UserName
+    icon_url: UserIconURL
+
+    @classmethod
+    def build_from_user(cls, user: User) -> "FeedbackCommentUser":
+        return cls(user_key=user.key, name=user.name, icon_url=user.icon_url)
 
 
 @dataclasses.dataclass(frozen=True)
