947600b5cfe3741ed7b2313a33cd66e2be4c2739
Akira Yumiyama <yumiyama.akira@gmail.com>
2020-10-03 19:38:08 +0900

    暫定のユーザ一覧ページを作成

--- server/src/common/user/presentation/user_list/__init__.py
+++ server/src/common/user/presentation/user_list/__init__.py
@@ -1,6 +1,12 @@
 import flask.views
 
+from common.user.domain.user_repository import UserRepository
+from framework.container import container
+
 
 class UserListPage(flask.views.MethodView):
     def get(self):
-        return "users"
+        user_repository: UserRepository = container.get(UserRepository)
+        users = user_repository.fetch_list()
+
+        return flask.render_template('common/user/users.html', users=users)
