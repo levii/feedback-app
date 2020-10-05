e4d857bee4b84340e6d457e220979b628423ae38
Akira Yumiyama <yumiyama.akira@gmail.com>
2020-10-05 15:43:30 +0900

    サポートユーザ向けのコメント投稿処理を実装

--- server/src/feedback/presentation/__init__.py
+++ server/src/feedback/presentation/__init__.py
@@ -1,6 +1,10 @@
 import flask
 
-from feedback.presentation.support import SupportFeedbackListView, SupportFeedbackView
+from feedback.presentation.support import (
+    SupportFeedbackListView,
+    SupportFeedbackView,
+    SupportFeedbackCommentView,
+)
 from feedback.presentation.user import (
     UserFeedbackListView,
     UserFeedbackView,
@@ -38,4 +42,9 @@ def register_feedback_blueprints(blueprint: flask.Blueprint) -> None:
         "/support/users/<user_id>/feedbacks/<feedback_id>",
         view_func=SupportFeedbackView.as_view("paths/support/feedback"),
         methods=["GET"],
+    )
+    blueprint.add_url_rule(
+        "/support/users/<user_id>/feedbacks/<feedback_id>/comments",
+        view_func=SupportFeedbackCommentView.as_view("paths/support/feedback/comments"),
+        methods=["POST"],
     )
--- server/src/feedback/presentation/support/__init__.py
+++ server/src/feedback/presentation/support/__init__.py
@@ -4,9 +4,14 @@ from werkzeug.exceptions import Forbidden
 from common.user.domain.key import UserKey
 from common.user.domain.user import SupportUser
 from common.user.domain.user_repository import UserRepository
+from feedback.application.support.feedback_comment_create_service import (
+    FeedbackCommentCreateService,
+)
 from feedback.application.support.feedback_fetch_service import FeedbackFetchService
 from feedback.application.support.feedbacks_fetch_service import FeedbacksFetchService
+from feedback.domain.comment import FeedbackCommentBody
 from feedback.domain.key import FeedbackKey
+from framework import utcnow_with_tz
 from framework.container import container
 
 
@@ -34,7 +39,9 @@ class SupportFeedbackListView(flask.views.MethodView):
 class SupportFeedbackView(flask.views.MethodView):
     def get(self, user_id: str, feedback_id: str) -> str:
         user_repository = container.get(UserRepository)
-        feedback_fetch_service: FeedbackFetchService = container.get(FeedbackFetchService)
+        feedback_fetch_service: FeedbackFetchService = container.get(
+            FeedbackFetchService
+        )
 
         user_key = UserKey.build(user_id)
         feedback_key = FeedbackKey.build(feedback_id)
@@ -42,11 +49,40 @@ class SupportFeedbackView(flask.views.MethodView):
         if not isinstance(current_user, SupportUser):
             raise Forbidden()
 
-        feedback = feedback_fetch_service.execute(user=current_user, feedback_key=feedback_key)
+        feedback = feedback_fetch_service.execute(
+            user=current_user, feedback_key=feedback_key
+        )
 
         return flask.render_template(
             "feedback/support/feedback.html",
             current_user=current_user,
             feedback=feedback.feedback,
             comments=feedback.comments,
+        )
+
+
+class SupportFeedbackCommentView(flask.views.MethodView):
+    def post(self, user_id: str, feedback_id: str) -> flask.Response:
+        user_repository = container.get(UserRepository)
+        feedback_comment_create_service: FeedbackCommentCreateService = container.get(
+            FeedbackCommentCreateService
+        )
+
+        user_key = UserKey.build(user_id)
+        feedback_key = FeedbackKey.build(feedback_id)
+        current_user = user_repository.fetch_by_key(user_key)
+        if not isinstance(current_user, SupportUser):
+            raise Forbidden()
+
+        feedback_comment_create_service.execute(
+            user=current_user,
+            feedback_key=feedback_key,
+            feedback_comment_body=FeedbackCommentBody(
+                f"サポートユーザからのコメントです - {utcnow_with_tz().isoformat()}"
+            ),
+        )
+
+        return flask.redirect(
+            location=f"/support/users/{user_id}/feedbacks/{feedback_id}",
+            Response=flask.Response,
         )
